b5a757a4bec11174f0b4ef58239c356b4cc52e3e
kiranrj <kiranrj@gmail.com>
2022-09-26 18:09:37 +0530

    if you don't do well. it'll be taken from you

--- src/metrics-ingest/routes/MetricsRecord.route.ts
+++ src/metrics-ingest/routes/MetricsRecord.route.ts
@@ -15,25 +15,7 @@ export class MetricRecordRoutes {
     }
 
     configureRoutes() {
-        this.app.get(`/data/metrics`, async(req: Request, res: Response) => {
-            try{
-                const records = await this.service.list();
-                res.status(200).send(records);
-            } catch (e) {
-                res.status(500).send(e);
-            }
-        });
-
-        this.app.get(`/data/metrics/:name`, async(req: Request, res: Response) => {
-            try{
-                const record = await this.service.get(req.params.name);
-                res.status(200).send(record);
-            } catch (e) {
-                res.status(500).send(e);
-            }
-        });
-
-        this.app.post(`/data/metrics/`, async(req: Request, res: Response) => {
+        this.app.post(`/ingest/metrics/`, async(req: Request, res: Response) => {
             try{
                 if (!req.body.name) {
                     console.log("Empty input. Skipping");
@@ -43,15 +25,6 @@ export class MetricRecordRoutes {
                 res.status(200).send(`Record ${req.body.name} UPDATED`);
             } catch (e: any) {
                 res.status(500).send(e.message);
-            }
-        });
-
-        this.app.delete(`/data/metrics/:name`, async(req: Request, res: Response) => {
-            try{
-                await this.service.delete(req.params.name);
-                res.status(200).send(`Record ${req.params.name} DELETED`);
-            } catch (e) {
-                res.status(500).send(e);
             }
         });
     }
